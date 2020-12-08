390a010624cbc531f1648c89e2c0cd5dc02d28c1
phoenix <phs28052001@gmail.com>
2020-12-08 12:56:57 +0530

    check.

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Animated from "react-native-reanimated"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Animated.Text>Testing the standalone build after installinig reanimated 2.0-alpha</Animated.Text>
       <StatusBar style="auto" />
     </View>
   );
