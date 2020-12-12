dec766cd2e1b45a504dafe21efd94c457a011725
phoenix <phs28052001@gmail.com>
2020-12-12 17:20:33 +0530

    Now working with vanilla animated.

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Animated from "react-native-reanimated"
+import { StyleSheet, Text, View, Animated, Button } from 'react-native';
 
 export default function App() {
+  const translateY = React.useRef(new Animated.Value(0)).current;
+
+  const goto = (end) => {
+    Animated.timing(translateY, {
+      toValue: end,
+      duration: 1500,
+      useNativeDriver: true
+    }).start();
+  }
   return (
     <View style={styles.container}>
-      <Animated.Text>Testing the standalone build after installing reanimated 2.0-release candidate - 0</Animated.Text>
-      <StatusBar style="auto" />
+      <View style={{ position: "absolute", left: 0, top: 0,right: 0, bottom:0, justifyContent: "space-between", zIndex: 10}}>
+        <Button
+          title="To bottom"
+          onPress={() => goto(400)}
+        />
+        <Button
+          title="To top"
+          onPress={() => goto(0)}
+        />
+      </View>
+      <Animated.Text style={{ margin: 50, transform: [{translateY}]}}>Testing the standalone build after installing reanimated 2.0-release candidate - 0</Animated.Text>
+      <StatusBar style="auto" hidden />
     </View>
   );
 }
@@ -16,7 +34,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
